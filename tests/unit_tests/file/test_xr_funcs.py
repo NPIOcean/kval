e3404202116cd0879bf2b95ac375645d59dc4b68
oyvfoss <oyvind.foss@npolar.no>
2024-08-09 13:41:44 +0200

    Moved metadata export function from file.xr_funcs to data.dataset. Updatet tests accordingly.

--- tests/unit_tests/file/test_xr_funcs.py
+++ tests/unit_tests/file/test_xr_funcs.py
@@ -2,11 +2,9 @@ import pytest
 import xarray as xr
 import pandas as pd
 import numpy as np
-from pathlib import Path
 from kval.file import xr_funcs  # Assuming the function is in xr_funcs module
 
 # Define a fixture for a mock dataset
-# Using this to check various functions on data and metadata
 @pytest.fixture
 def mock_dataset() -> xr.Dataset:
     """
@@ -129,50 +127,3 @@ def test_pick_multiple_conditions(mock_dataset):
     # Check if `PRES` dimension is correctly filtered
     assert (result.PRES.values == [100, 2000]).all()
 
-
-
-def test_metadata_to_txt(mock_dataset):
-    """
-    Test the metadata_to_txt function using a mock dataset and temporary file.
-    """
-    # Define the output file path
-    outfile = 'metadata_output.txt'
-    
-    # Call the function to create the metadata file
-    xr_funcs.metadata_to_txt(mock_dataset, outfile)
-    
-    # Read the content of the file and verify its correctness
-    with open(outfile, 'r') as f:
-        content = f.read()
-    
-    # Check for expected strings in the output file
-    assert 'FILE METADATA FROM: test_dataset' in content
-    assert '### GLOBAL ATTRIBUTES   ###' in content
-    assert 'description:\nThis is a test dataset.' in content
-    assert 'author:\nTest Author' in content
-    assert '### VARIABLE ATTRIBUTES ###' in content
-    assert 'TEMP' in content
-    assert 'units:\ndegC' in content
-    assert 'long_name:\nTest Temperature' in content
-    
-
-    # Verify that attributes appear directly after the correct variable name
-    temp_index = content.index('TEMP')
-    ocean_index = content.index('OCEAN')
-    station_index = content.index('STATION')
-
-    # Check that the order is right
-    assert temp_index < ocean_index
-    assert ocean_index < station_index
-
-    # Check that we have one unit entry and one long_name entry under TEMP 
-    assert content[temp_index:ocean_index].count('units:') == 1
-    assert content[temp_index:ocean_index].count('long_name:') == 1
-
-    # Check that we have no unit entry or one long_name entry under OCEAN 
-    assert content[ocean_index:station_index].count('units:') == 0
-    assert content[ocean_index:station_index].count('long_name:') == 0
-
-
-    # Clean up the file after test
-    Path(outfile).unlink()
