5a2180d36a1067ed127e1a4ceb1f776e6b9fc734
oyvfoss <oyvind.foss@npolar.no>
2024-08-13 15:40:28 +0200

    Modified test_imports to ignore warning
    - Changed the structure of the test script; better now.
    - Ignoring a deprecation warning about the seawater library (we need to stick with it for now as there is no equivalent of the seawater.eos80.salt() function in gsw)

--- tests/functional_tests/test_imports.py
+++ tests/functional_tests/test_imports.py
@@ -1,10 +1,13 @@
-from kval.calc import numbers
-from kval.data import edit, dataset, ctd
-from kval.data.ship_ctd_tools import _ctd_edit, _ctd_tools, _ctd_visualize, qc_salt
-from kval.file import sbe, rbr, matfile,  _variable_defs
-from kval.geo import geocalc
-from kval.maps import quickmap
-from kval.metadata import conventionalize, check_conventions, _standard_attrs, _standard_attrs_org
-from kval.ocean import empirical, uv
-from kval.util import  time, user_input, xr_funcs
+import pytest
 
+@pytest.mark.filterwarnings("ignore:The seawater library is deprecated! Please use gsw instead.")
+def test_imports():
+    from kval.calc import numbers
+    from kval.data import edit, dataset, ctd
+    from kval.data.ship_ctd_tools import _ctd_edit, _ctd_tools, _ctd_visualize, qc_salt
+    from kval.file import sbe, rbr, matfile,  _variable_defs
+    from kval.geo import geocalc
+    from kval.maps import quickmap
+    from kval.metadata import conventionalize, check_conventions, _standard_attrs, _standard_attrs_org
+    from kval.ocean import empirical, uv
+    from kval.util import time, user_input, xr_funcs
