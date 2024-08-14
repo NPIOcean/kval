104eace58988ebc0bae80d62fb97ee93fe3ec6e9
oyvfoss <oyvind.foss@npolar.no>
2024-08-14 16:44:18 +0200

    Try to make convention-checker anm optional dependency

--- src/kval/metadata/check_conventions.py
+++ src/kval/metadata/check_conventions.py
@@ -1,17 +1,31 @@
-from compliance_checker.runner import ComplianceChecker, CheckSuite
 import os
 import xarray as xr
 from IPython.display import display, clear_output
 import ipywidgets as widgets
 
+# Conditional import for compliance-checker
+try:
+    from compliance_checker.runner import ComplianceChecker, CheckSuite
+    COMPLIANCE_CHECKER_AVAILABLE = True
+except ImportError:
+    COMPLIANCE_CHECKER_AVAILABLE = False
+
 def check_file(file):
     '''
     Use the IOOS compliance checker 
     (https://github.com/ioos/compliance-checker-web)
-    to chek an nc file (CF and ACDD conventions).
+    to check an nc file (CF and ACDD conventions).
 
     Can take a file path or an xr.Dataset as input
     '''
+    if not COMPLIANCE_CHECKER_AVAILABLE:
+        raise ImportError("IOOS Compliance Checker is not installed. "
+            "Please install it to use this functionality, e.g.:\n"
+            "$ conda install -c conda-forge compliance-checker\nor\n"
+            "$ pip install compliance-checker\n\n"
+            "(Note: There are some issues with getting the dependencies "
+            " of this library to work on Py3.12, MacOS and Windows..)")
+    
     # Load all available checker classes
     temp = False
     if isinstance(file, xr.Dataset):
@@ -39,19 +53,20 @@ def check_file(file):
     if temp:
         os.remove(temp_file)
 
-
-
 def check_file_with_button(file):
     '''
     (Wrapper for check_file() with a "close" button)
 
     Use the IOOS compliance checker 
     (https://github.com/ioos/compliance-checker-web)
-    to chek an nc file (CF and ACDD conventions).
+    to check an nc file (CF and ACDD conventions).
 
     Can take a file path or an xr.Dataset as input
     '''
 
+    if not COMPLIANCE_CHECKER_AVAILABLE:
+        raise ImportError("Compliance Checker is not installed. Please install it to use this functionality.")
+    
     output_widget = widgets.Output()
 
     def on_button_click(b):
@@ -66,7 +81,7 @@ def check_file_with_button(file):
     close_button = widgets.Button(description="Close")
     close_button.on_click(on_button_click)
 
-    display(widgets.VBox([close_button, output_widget, ]))
+    display(widgets.VBox([close_button, output_widget]))
 
     # Your existing code
     with output_widget:
