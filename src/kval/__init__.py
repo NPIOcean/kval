25ef70d7ca62967b86258fbcd15e20199bc368f6
oyvfoss <oyvind.foss@npolar.no>
2025-11-26 10:35:16 +0100

    Improved mpl warnign message

--- src/kval/__init__.py
+++ src/kval/__init__.py
@@ -7,7 +7,7 @@ def _check_widget_compatibility():
     mver = tuple(map(int, matplotlib.__version__.split('.')[:2]))
     if mver >= (3, 9):
         warnings.warn(
-            "Matplotlib >=3.9 may in some cases break ipympl interactive plots. "
+            "NOTE_ Matplotlib >=3.9 may *in some cases* break some ipympl interactive plots. "
             "If you experience trouble with plots not displaying correctly, try downgrading to matplotlib 3.8.x.",
             UserWarning
         )
