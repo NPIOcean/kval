8428db4abe070c3ff33fb8bc33427c1eca832fb4
oyvfoss <oyvind.foss@npolar.no>
2025-11-26 10:26:25 +0100

    removed jupyter dependency. Added check of mpl version on import (recommending <3.9)

--- src/kval/__init__.py
+++ src/kval/__init__.py
@@ -1,1 +1,17 @@
-# oceanograPy
+# kval
+
+import matplotlib
+
+def _check_widget_compatibility():
+    import warnings
+    mver = tuple(map(int, matplotlib.__version__.split('.')[:2]))
+    if mver >= (3, 9):
+        warnings.warn(
+            "Matplotlib >=3.9 may in some cases break ipympl interactive plots. "
+            "If you experience trouble with plots not displaying correctly, try downgrading to matplotlib 3.8.x.",
+            UserWarning
+        )
+
+
+# Run check automatically when this module is imported
+_check_widget_compatibility()
